4348c02a935c33a4d61922cfb9ed16219a16ba01
sawel <sawel@yandex.ru>
2020-06-28 14:38:40 +0800

    Initial commit

---
+++ src/SimpleTreeNode.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+public class SimpleTreeNode<T> {
+    public T NodeValue; // значение в узле
+    public SimpleTreeNode<T> Parent; // родитель или null для корня
+    public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+    public SimpleTreeNode(T val, SimpleTreeNode<T> parent) {
+        NodeValue = val;
+        Parent = parent;
+        Children = new ArrayList<>();
+    }
+}
+
+class SimpleTree<T> {
+    public SimpleTreeNode<T> Root; // корень, может быть null
+
+    public SimpleTree(SimpleTreeNode<T> root) {
+        Root = root;
+    }
+
+    public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild) {
+        // ваш код добавления нового дочернего узла существующему ParentNode
+        NewChild.Parent = ParentNode;
+        ParentNode.Children.add(NewChild);
+    }
+
+    public void DeleteNode(SimpleTreeNode<T> NodeToDelete) {
+        // ваш код удаления существующего узла NodeToDelete
+        SimpleTreeNode<T> parent = NodeToDelete.Parent;
+        parent.Children.remove(NodeToDelete);
+        // parent.Children.addAll(NodeToDelete.Children);
+        // NodeToDelete.Children.clear();
+    }
+
+    public List<SimpleTreeNode<T>> GetAllNodes() {
+        // ваш код выдачи всех узлов дерева в определённом порядке
+        SimpleTreeNode<T> simpleTreeNode;
+        List<SimpleTreeNode<T>> listResult = new ArrayList<>();
+        List<SimpleTreeNode<T>> listTemp = new ArrayList<>();
+        listTemp.add(Root);
+
+        while (!listTemp.isEmpty()) {
+            simpleTreeNode = listTemp.remove(0);
+            if (!simpleTreeNode.Children.isEmpty()) {
+                //System.out.println("test - " + simpleTreeNode.Children);
+                listTemp.addAll(simpleTreeNode.Children);
+                //System.out.println();
+            }
+            listResult.add(simpleTreeNode);
+        }
+        return listResult;
+    }
+
+    public List<SimpleTreeNode<T>> FindNodesByValue(T val) {
+        // ваш код поиска узлов по значению
+        List<SimpleTreeNode<T>> listResult = new ArrayList<>();
+        for (SimpleTreeNode<T> i :
+                GetAllNodes()) {
+            if (i.NodeValue == val) listResult.add(i);
+        }
+        return listResult;
+    }
+
+    public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent) {
+        // ваш код перемещения узла вместе с его поддеревом --
+        // в качестве дочернего для узла NewParent
+        DeleteNode(OriginalNode);
+        AddChild(NewParent, OriginalNode);
+    }
+
+    public int Count() {
+        // количество всех узлов в дереве
+        return GetAllNodes().size();
+    }
+
+    public int LeafCount() {
+        // количество листьев в дереве
+        int count = 0;
+        for (SimpleTreeNode<T> i :
+                GetAllNodes()) {
+            if (i.Children.isEmpty()) count ++;
+        }
+        return count;
+    }
+
+  /*  private List<SimpleTreeNode<T>> EnumTree(SimpleTreeNode<T> root) {
+        List<SimpleTreeNode<T>> list = new ArrayList<>();
+
+
+    }*/
+}
